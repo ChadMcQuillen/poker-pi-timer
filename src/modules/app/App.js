fa60f3166e0f511a991d8620b7e54db1ef488348
Chad McQuillen <chad@bannerstonesoftware.com>
2020-02-16 19:34:53 -0500

    Convert App to a functional component.

--- src/modules/app/App.js
+++ src/modules/app/App.js
@@ -1,61 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import TimerTickService from 'core/timer-tick-service';
 import GraphQLTournamentService from 'core/graphql-tournament-control-service';
 import Tournament from 'core/tournament';
 import { TournamentBoardView } from 'modules/tournament-board';
 
-export class App extends React.Component {
-    constructor( props ) {
-        super( props );
-        this.state = { };
-    }
+export const App = ( props ) => {
 
-    componentDidMount() {
-        this.tournament = new Tournament( new TimerTickService(), new GraphQLTournamentService() );
-        this.tournament.tournament.subscribe(
-            value => {
-                var newState = { ...this.state, ...value };
-                this.setState( newState );
-            }
-        )
-    }
+  const [ isLoading, setIsLoading ] = useState( true );
+  const [ tournament, setTournament ] = useState( {} );
 
-    render() {
-        if ( this.state.description == null ) {
-            return (
-              <div>Loading...</div>
-            )
-        } else {
-            const {
-                title,
-                description,
-                buyIn,
-                rebuyAmount,
-                rebuyThroughLevel,
-                levelsAndBreaks,
-                currentLevelIndex,
-                numberOfEntrants,
-                numberOfPlayersRemaining,
-                numberOfRebuys,
-                payouts,
-                secondsRemaining
-            } = this.state;
-            return (
-              <TournamentBoardView
-                title = { title }
-                description = { description }
-                buyIn = { buyIn }
-                rebuyAmount = { rebuyAmount }
-                rebuyThroughLevel = { rebuyThroughLevel }
-                numberOfEntrants = { numberOfEntrants }
-                numberOfPlayersRemaining = { numberOfPlayersRemaining }
-                numberOfRebuys = { numberOfRebuys }
-                currentLevelIndex = { currentLevelIndex }
-                levelsAndBreaks = { levelsAndBreaks }
-                payouts = { payouts }
-                secondsRemaining = { secondsRemaining } />
-            );
+  useEffect( () => {
+    const receiveTournamentUpdates = () => {
+      const tournamentService = new Tournament( new TimerTickService(), new GraphQLTournamentService() );
+      tournamentService.tournament.subscribe(
+        value => {
+          setTournament( prev => {
+            return { ...prev, ...value };
+          });
+          if ( isLoading && value.hasOwnProperty( 'state' ) ) {
+            setIsLoading( false );
+          }
         }
-    }
-}
+      )
+    };
+    receiveTournamentUpdates();
+  }, [] );
+
+  const withLoadingIndicator = ( Component ) => {
+    return function EnhancedComponent( { isLoading, ...props } ) {
+      if ( !isLoading ) {
+        return <Component { ...props } />;
+      }
+      return (
+        <div>Loading...</div>
+      );
+    };
+  };
+
+  const TournamentBoardWithLoadingIndicator = withLoadingIndicator( TournamentBoardView );
+
+  return (
+    <TournamentBoardWithLoadingIndicator
+      isLoading = { isLoading }
+      title = { tournament.title }
+      description = { tournament.description }
+      buyIn = { tournament.buyIn }
+      rebuyAmount = { tournament.rebuyAmount }
+      rebuyThroughLevel = { tournament.rebuyThroughLevel }
+      numberOfEntrants = { tournament.numberOfEntrants }
+      numberOfPlayersRemaining = { tournament.numberOfPlayersRemaining }
+      numberOfRebuys = { tournament.numberOfRebuys }
+      currentLevelIndex = { tournament.currentLevelIndex }
+      levelsAndBreaks = { tournament.levelsAndBreaks }
+      payouts = { tournament.payouts }
+      secondsRemaining = { tournament.secondsRemaining } />
+  );
+};
